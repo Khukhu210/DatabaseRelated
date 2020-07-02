18a0769663f824beb4c8c8c02afe722b0fce68ff
khukhu210 <bitkhushboo210@gmail.com>
2020-07-02 13:06:19 +0530

    Space Added to check the commit

--- HibernateDemo/src/main/java/com/learning/hibernate/database/HibernateDemo/HibernateDemoApplication.java
+++ HibernateDemo/src/main/java/com/learning/hibernate/database/HibernateDemo/HibernateDemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HibernateDemoApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(HibernateDemoApplication.class, args);
 	}
 
